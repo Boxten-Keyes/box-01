50004e33bdb16401bfbb2c7fff72e0ce04c0fd4a
Boxten <boxtenkeyes@gmail.com>
2025-06-19 21:13:07 -0400

    Update box#[loadstrings].lua

--- box#[loadstrings].lua
+++ box#[loadstrings].lua
@@ -1,154 +1,22 @@
 --[[---------------------------------------------------------------------------------------------------------------------------
 
-DANDY'S WORLD
+NOXIOUS HUB LOADERS
 
 ---------------------------------------------------------------------------------------------------------------------------]]--
 
 -- DANDY'S WORLD [MAIN]
 function grs(l) local chars = {"▓", "▒", "░"};local result = "";for i = 1, l do local rndi = math.random(1, #chars);result = result .. chars[rndi];end;return result end
 function ctc(txt) LOLOL = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set); if LOLOL then LOLOL(tostring(txt)) end end
-a = "https://";
-b, c = "raw.", "githubusercontent.com";
-d, e, f = "Boxten-Keyes", "nocturnal", "refs/heads/main";
-g = "dandy's%20world/%5Bobf%5D%20%5Bmain%5D%20noxious%20hub";
-h = ".lua";
-i = g .. h;
-j = true and "-obfuscated" or "";
-k = a .. b .. c .. "/" .. d .. "/" .. e .. j .. "/" .. f .. "/" .. i;
+a = "https://"
+b, c = "raw.", "githubusercontent.com"
+d, e, f = "Boxten-Keyes", "box-01", "refs/heads/main"
+g = "box%23%5Bnoxious%20hub%3A%20dandy's%20world%5D/box%23%5Bmain%5D"
+h = ".lua"
+i = g .. h
+j = a .. b .. c .. "/" .. d .. "/" .. e .. "/" .. f .. "/" .. i
 task.spawn(function() local LOL = true task.spawn(function() while LOL do local A = grs(222) print(A) ctc(A) task.wait() end end) task.wait(5) LOL = false end)
-lod = getfenv(0).loadstring or loadstring;
-src = game:HttpGet(k);lod(src)();
-
--- DANDY'S WORLD [ANIMATIONS GUI]
-function grs(l) local chars = {"▓", "▒", "░"};local result = "";for i = 1, l do local rndi = math.random(1, #chars);result = result .. chars[rndi];end;return result end
-function ctc(txt) LOLOL = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set); if LOLOL then LOLOL(tostring(txt)) end end
-a = "https://";
-b, c = "raw.", "githubusercontent.com";
-d, e, f = "x2-x21-x18-x14-x5-x18", "nocturnal", "refs/heads/main";
-g = "dandy's%20world/%5Bobf%5D%20animations%20gui";
-h = ".lua";
-i = g .. h;
-j = true and "-obfuscated" or "";
-k = a .. b .. c .. "/" .. d .. "/" .. e .. j .. "/" .. f .. "/" .. i;
-task.spawn(function() local LOL = true task.spawn(function() while LOL do local A = grs(70) print(A) ctc(A) task.wait() end end) task.wait(20) LOL = false end)
-lod = getfenv(0).loadstring or loadstring;
-src = game:HttpGet(k);lod(src)();
-
--- DANDY'S WORLD [AUTOFARM GUI]
-function grs(l) local chars = {"▓", "▒", "░"};local result = "";for i = 1, l do local rndi = math.random(1, #chars);result = result .. chars[rndi];end;return result end
-function ctc(txt) LOLOL = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set); if LOLOL then LOLOL(tostring(txt)) end end
-a = "https://";
-b, c = "raw.", "githubusercontent.com";
-d, e, f = "x2-x21-x18-x14-x5-x18", "nocturnal", "refs/heads/main";
-g = "dandy's%20world/%5Bobf%5D%20autofarm%20gui";
-h = ".lua";
-i = g .. h;
-j = true and "-obfuscated" or "";
-k = a .. b .. c .. "/" .. d .. "/" .. e .. j .. "/" .. f .. "/" .. i;
-task.spawn(function() local LOL = true task.spawn(function() while LOL do local A = grs(70) print(A) ctc(A) task.wait() end end) task.wait(20) LOL = false end)
-lod = getfenv(0).loadstring or loadstring;
-src = game:HttpGet(k);lod(src)();
-
---[[---------------------------------------------------------------------------------------------------------------------------
-
-FORSAKEN
-
----------------------------------------------------------------------------------------------------------------------------]]--
-
--- FORSAKEN [MAIN]
-function grs(l) local chars = {"▓", "▒", "░"};local result = "";for i = 1, l do local rndi = math.random(1, #chars);result = result .. chars[rndi];end;return result end
-function ctc(txt) LOLOL = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set); if LOLOL then LOLOL(tostring(txt)) end end
-a = "https://";
-b, c = "raw.", "githubusercontent.com";
-d, e, f = "x2-x21-x18-x14-x5-x18", "nocturnal", "refs/heads/main";
-g = "forsaken/%5Bobf%5D%20%5Bmain%5D%20noxious%20hub";
-h = ".lua";
-i = g .. h;
-j = true and "-obfuscated" or "";
-k = a .. b .. c .. "/" .. d .. "/" .. e .. j .. "/" .. f .. "/" .. i;
-task.spawn(function() local LOL = true task.spawn(function() while LOL do local A = grs(70) print(A) ctc(A) task.wait() end end) task.wait(20) LOL = false end)
-lod = getfenv(0).loadstring or loadstring;
-src = game:HttpGet(k);lod(src)();
-
---[[---------------------------------------------------------------------------------------------------------------------------
-
-MISCELLANEOUS
-
----------------------------------------------------------------------------------------------------------------------------]]--
-
--- MISCELLANEOUS [CUSTOM ORBITER GUI]
-function grs(l) local chars = {"▓", "▒", "░"};local result = "";for i = 1, l do local rndi = math.random(1, #chars);result = result .. chars[rndi];end;return result end
-function ctc(txt) LOLOL = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set); if LOLOL then LOLOL(tostring(txt)) end end
-a = "https://";
-b, c = "raw.", "githubusercontent.com";
-d, e, f = "x2-x21-x18-x14-x5-x18", "nocturnal", "refs/heads/main";
-g = "miscellaneous/%5Bobf%5D%20custom%20orbiter%20gui";
-h = ".lua";
-i = g .. h;
-j = true and "-obfuscated" or "";
-k = a .. b .. c .. "/" .. d .. "/" .. e .. j .. "/" .. f .. "/" .. i;
-task.spawn(function() local LOL = true task.spawn(function() while LOL do local A = grs(70) print(A) ctc(A) task.wait() end end) task.wait(20) LOL = false end)
-lod = getfenv(0).loadstring or loadstring;
-src = game:HttpGet(k);lod(src)();
-
--- MISCELLANEOUS [FLIP TOOLS]
-function grs(l) local chars = {"▓", "▒", "░"};local result = "";for i = 1, l do local rndi = math.random(1, #chars);result = result .. chars[rndi];end;return result end
-function ctc(txt) LOLOL = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set); if LOLOL then LOLOL(tostring(txt)) end end
-a = "https://";
-b, c = "raw.", "githubusercontent.com";
-d, e, f = "x2-x21-x18-x14-x5-x18", "nocturnal", "refs/heads/main";
-g = "miscellaneous/%5Bobf%5D%20frontflip%20and%20backflip%20tools";
-h = ".lua";
-i = g .. h;
-j = true and "-obfuscated" or "";
-k = a .. b .. c .. "/" .. d .. "/" .. e .. j .. "/" .. f .. "/" .. i;
-task.spawn(function() local LOL = true task.spawn(function() while LOL do local A = grs(70) print(A) ctc(A) task.wait() end end) task.wait(20) LOL = false end)
-lod = getfenv(0).loadstring or loadstring;
-src = game:HttpGet(k);lod(src)();
-
--- MISCELLANEOUS [FLOAT GUI]
-function grs(l) local chars = {"▓", "▒", "░"};local result = "";for i = 1, l do local rndi = math.random(1, #chars);result = result .. chars[rndi];end;return result end
-function ctc(txt) LOLOL = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set); if LOLOL then LOLOL(tostring(txt)) end end
-a = "https://";
-b, c = "raw.", "githubusercontent.com";
-d, e, f = "x2-x21-x18-x14-x5-x18", "nocturnal", "refs/heads/main";
-g = "miscellaneous/%5Bobf%5D%20float%20gui";
-h = ".lua";
-i = g .. h;
-j = true and "-obfuscated" or "";
-k = a .. b .. c .. "/" .. d .. "/" .. e .. j .. "/" .. f .. "/" .. i;
-task.spawn(function() local LOL = true task.spawn(function() while LOL do local A = grs(70) print(A) ctc(A) task.wait() end end) task.wait(20) LOL = false end)
-lod = getfenv(0).loadstring or loadstring;
-src = game:HttpGet(k);lod(src)();
-
--- MISCELLANEOUS [SING THICK OF IT]
-function grs(l) local chars = {"▓", "▒", "░"};local result = "";for i = 1, l do local rndi = math.random(1, #chars);result = result .. chars[rndi];end;return result end
-function ctc(txt) LOLOL = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set); if LOLOL then LOLOL(tostring(txt)) end end
-a = "https://";
-b, c = "raw.", "githubusercontent.com";
-d, e, f = "x2-x21-x18-x14-x5-x18", "nocturnal", "refs/heads/main";
-g = "miscellaneous/%5Bobf%5D%20sing%20thick%20of%20it";
-h = ".lua";
-i = g .. h;
-j = true and "-obfuscated" or "";
-k = a .. b .. c .. "/" .. d .. "/" .. e .. j .. "/" .. f .. "/" .. i;
-task.spawn(function() local LOL = true task.spawn(function() while LOL do local A = grs(70) print(A) ctc(A) task.wait() end end) task.wait(20) LOL = false end)
-lod = getfenv(0).loadstring or loadstring;
-src = game:HttpGet(k);lod(src)();
-
--- MISCELLANEOUS [SPEED CHANGER GUI]
-function grs(l) local chars = {"▓", "▒", "░"};local result = "";for i = 1, l do local rndi = math.random(1, #chars);result = result .. chars[rndi];end;return result end
-function ctc(txt) LOLOL = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set); if LOLOL then LOLOL(tostring(txt)) end end
-a = "https://";
-b, c = "raw.", "githubusercontent.com";
-d, e, f = "x2-x21-x18-x14-x5-x18", "nocturnal", "refs/heads/main";
-g = "miscellaneous/%5Bobf%5D%20speed%20changer%20gui";
-h = ".lua";
-i = g .. h;
-j = true and "-obfuscated" or "";
-k = a .. b .. c .. "/" .. d .. "/" .. e .. j .. "/" .. f .. "/" .. i;
-task.spawn(function() local LOL = true task.spawn(function() while LOL do local A = grs(70) print(A) ctc(A) task.wait() end end) task.wait(20) LOL = false end)
-lod = getfenv(0).loadstring or loadstring;
-src = game:HttpGet(k);lod(src)();
+lod = getfenv(0).loadstring or loadstring
+src = game:HttpGet(j)
+lod(src)()
 
 -------------------------------------------------------------------------------------------------------------------------------
