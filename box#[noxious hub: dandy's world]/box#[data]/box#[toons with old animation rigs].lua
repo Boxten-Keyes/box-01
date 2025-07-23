54ddb9a640bddda4045ef17bba17342a75aef5b0
Boxten <boxtenkeyes@gmail.com>
2025-07-23 19:33:53 -0400

    Create box#[toons with old animation rigs].lua

---
+++ box#[noxious hub: dandy's world]/box#[data]/box#[toons with old animation rigs].lua
@@ -0,0 +1,4 @@
+return { "Boxten", "Brightney", "Cosmo", "Finn", "Flutter", "Gigi",
+  "Glisten", "Goob", "Poppy", "Rodger", "Scraps", "Shrimpo", "Teagan", 
+  "Tisha", "Toodles", "Astro", "Shelly", "Sprout", "Dandy",
+}
