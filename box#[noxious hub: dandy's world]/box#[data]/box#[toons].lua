9e5e9de5288ba7e19fdedf6a706cefa0e305738a
Boxten <boxtenkeyes@gmail.com>
2025-07-23 19:32:18 -0400

    Create box#[toons].lua

---
+++ box#[noxious hub: dandy's world]/box#[data]/box#[toons].lua
@@ -0,0 +1,7 @@
+return { "Blott", "Flyte", "Yatta", "Bassie", "Cocoa", "Eggson", 
+  "Bobette", "Coal", "Rudie", "Ginger", "Boxten", "Rudie", "Brightney",
+  "Cosmo", "Connie", "Finn", "Flutter", "Gigi", "Glisten", "Goob",
+  "Poppy", "RazzleDazzle", "Rodger", "Scraps", "Shrimpo", "Teagan", 
+  "Tisha", "Toodles", "Sprout", "Vee", "Dandy", "Shelly", "Pebble",
+  "Astro", "Dandy", "Looey"
+}
